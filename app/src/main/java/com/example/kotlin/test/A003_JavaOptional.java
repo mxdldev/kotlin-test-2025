4c11e7062decfdfba872ae492f922346d55c8887
guoxl <guoxl@pep.com.cn>
2025-02-20 17:13:23 +0800

    firest commit

--- app/src/main/java/com/example/kotlin/test/A003_JavaOptional.java
+++ app/src/main/java/com/example/kotlin/test/A003_JavaOptional.java
@@ -1,6 +1,8 @@
 package com.example.kotlin.test;
 
 
+import android.text.TextUtils;
+
 import java.util.Optional;
 
 /**
@@ -13,8 +15,11 @@ import java.util.Optional;
 public class A003_JavaOptional {
     public static void main(String[] args) {
         String name = "Jack";
+        //对于name字符串构建了一个Optional对象实体
         Optional<String> optional = Optional.of(name);
+        //判断是不是存在，传递了一个lambda，一行代码搞定
         optional.ifPresent(value -> System.out.println(value));
+        //传统方法，相当于TextUtils.isEmpty()函数
         if(optional.isPresent()){
             System.out.println(optional.get());
         }
